1d7b7e76b429e7916c5fa7ae8c0bea0e511315b6
yexulong <yexulong@foxmail.com>
2025-09-15 02:00:22 +0800

    Enhanced Input

---
+++ Source/test3/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright yxl
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/test3/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright yxl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST3_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
