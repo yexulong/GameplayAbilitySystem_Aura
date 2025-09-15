f3e262a8fa2e2494b88799862921407819b78b9a
yexulong <yexulong@foxmail.com>
2025-09-15 23:16:36 +0800

    The Player State

---
+++ Source/test3/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright yxl
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/test3/Public/Player/AuraPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright yxl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AuraPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST3_API AAuraPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerState();
+	
+};
