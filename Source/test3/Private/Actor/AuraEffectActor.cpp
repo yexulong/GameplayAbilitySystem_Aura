ae3df39c9897c556c2ef40f4410805a042782065
yexulong <yexulong@foxmail.com>
2025-09-21 17:38:25 +0800

    Effect Actor Improved

--- Source/test3/Private/Actor/AuraEffectActor.cpp
+++ Source/test3/Private/Actor/AuraEffectActor.cpp
@@ -5,9 +5,6 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/AuraAbilitySystemComponent.h"
-#include "AbilitySystem/AuraAttributeSet.h"
 
 AAuraEffectActor::AAuraEffectActor()
 {
