887f7c1373fa768f40190bc09a4edce31c9a5c0d
yexulong <yexulong@foxmail.com>
2025-09-23 23:40:36 +0800

    PreAttributeChange

--- Source/test3/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/test3/Public/AbilitySystem/AuraAttributeSet.h
@@ -23,6 +23,8 @@ class TEST3_API UAuraAttributeSet : public UAttributeSet
 public:
 	UAuraAttributeSet();
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
