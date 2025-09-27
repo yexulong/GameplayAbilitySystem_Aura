efc2ed65cfc0c4c412c82226788dc7add3edf2e4
yexulong <yexulong@foxmail.com>
2025-09-27 20:46:11 +0800

    Initialize Attributes with Gameplay Effects

--- Source/test3/Private/Character/AuraCharacter.cpp
+++ Source/test3/Private/Character/AuraCharacter.cpp
@@ -54,5 +54,6 @@ void AAuraCharacter::InitAbilityActorInfo()
 		{
 			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
 		}
-	} 
+	}
+	InitializePrimaryAttributes();
 }
