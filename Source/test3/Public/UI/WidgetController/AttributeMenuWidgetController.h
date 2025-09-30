e04cad51c8fd6e01098383b2b59e3e2825170286
yexulong <yexulong@foxmail.com>
2025-09-30 21:44:05 +0800

    Attribute Menu Widget Controller

---
+++ Source/test3/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -0,0 +1,19 @@
+// Copyright yxl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST3_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BindCallbacksToDependencies() override;
+	virtual void BroadcastInitialValues() override;
+};
