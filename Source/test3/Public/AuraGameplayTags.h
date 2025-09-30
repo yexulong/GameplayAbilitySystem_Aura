d5a9248c9ff9133814f340edbae3ae40dbe6217e
wb.yexulong <wb.yexulong@mesg.corp.netease.com>
2025-09-30 16:59:35 +0800

    Gameplay Tags Singleton

---
+++ Source/test3/Public/AuraGameplayTags.h
@@ -0,0 +1,21 @@
+// Copyright yxl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native Gameplay Tags
+ */
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; };
+	static void InitializeNativeGameplayTags();
+protected:
+
+private:
+	static FAuraGameplayTags GameplayTags;
+};
