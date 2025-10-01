a7c9a1fc4944f0b28d48de14d5cee150c3bdeaa7
yexulong <yexulong@foxmail.com>
2025-10-01 21:02:55 +0800

    Responding to Attribute Changes

--- Source/test3/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/test3/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -28,4 +28,7 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };
