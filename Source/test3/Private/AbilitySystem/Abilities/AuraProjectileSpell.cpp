3ac661feff6f2f5e376691fb3fdae1a1d84d158e
yexulong <yexulong@foxmail.com>
2025-10-05 14:22:34 +0800

    Spawn FireBolt from Event

--- Source/test3/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/test3/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -12,7 +12,11 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+}
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
--- Source/test3/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/test3/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -17,6 +17,9 @@ class TEST3_API UAuraProjectileSpell : public UAuraGameplayAbility
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
+	UFUNCTION(BlueprintCallable, Category = "Projectile")
+	void SpawnProjectile();
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;
 };
