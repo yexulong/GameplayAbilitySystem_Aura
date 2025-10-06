4ad7180562c6db01acc0caf314b49fc6edcc9299
yexulong <yexulong@foxmail.com>
2025-10-07 02:16:10 +0800

    Initializing Enemy Attributes

--- Source/test3/Private/Character/AuraEnemy.cpp
+++ Source/test3/Private/Character/AuraEnemy.cpp
@@ -7,6 +7,7 @@
 #include <AbilitySystem/AuraAbilitySystemComponent.h>
 #include <AbilitySystem/AuraAttributeSet.h>
 
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Components/WidgetComponent.h"
 #include "UI/Widget/AuraUserWidget.h"
 
@@ -80,4 +81,9 @@ void AAuraEnemy::InitAbilityActorInfo()
 	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->AbilityActorInfoSet();
 
 	InitializeDefaultAttributes();
+}
+
+void AAuraEnemy::InitializeDefaultAttributes() const
+{
+	UAuraAbilitySystemLibrary::InitializeDefaultAttributes(this, CharacterClass, Level, AbilitySystemComponent);
 }
--- Source/test3/Public/Game/AuraGameModeBase.h
+++ Source/test3/Public/Game/AuraGameModeBase.h
@@ -6,6 +6,7 @@
 #include "GameFramework/GameModeBase.h"
 #include "AuraGameModeBase.generated.h"
 
+class UCharacterClassInfo;
 /**
  * 
  */
@@ -13,5 +14,7 @@ UCLASS()
 class TEST3_API AAuraGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Character Class Defatuls")
+	TObjectPtr<UCharacterClassInfo> CharacterClassInfo;
 };
