5c402549abb2c57588fbcbbba7cbf1a9f709cacc
yexulong <yexulong@foxmail.com>
2025-10-07 16:08:29 +0800

    Damage Meta Attribute

--- Source/test3/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/test3/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -127,6 +127,18 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 	{
 		SetMana(FMath::Clamp<float>(GetMana(), 0.0f, GetMaxMana()));
 	}
+	if (Data.EvaluatedData.Attribute == GetIncomingDamageAttribute())
+	{
+		const float LocalIncomingDamage = GetIncomingDamage();
+		SetIncomingDamage(0.f);
+		if (LocalIncomingDamage > 0)
+		{
+			const float NewHealth = GetHealth() - LocalIncomingDamage;
+			SetHealth(FMath::Clamp<float>(NewHealth, 0.0f, GetMaxHealth()));
+
+			const bool bFatal = NewHealth <= 0.0f;
+		}
+	}
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
