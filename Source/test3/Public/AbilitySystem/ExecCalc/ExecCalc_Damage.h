4e3aaab66ebf7cd111a6077eaae59fdbe9253249
yexulong <yexulong@foxmail.com>
2025-10-16 22:55:06 +0800

    Damage Execution Calculation

---
+++ Source/test3/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,20 @@
+// Copyright yxl
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST3_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+public:
+	UExecCalc_Damage();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+};
