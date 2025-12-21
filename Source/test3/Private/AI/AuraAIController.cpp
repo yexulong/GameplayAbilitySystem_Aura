d94a76e873043ca9246665dfc8916c8283027193
yexulong <yexulong@foxmail.com>
2025-12-22 00:25:47 +0800

    AI Controller, Blackboard, and Behavior Tree

---
+++ Source/test3/Private/AI/AuraAIController.cpp
@@ -0,0 +1,15 @@
+// Copyright yxl
+
+
+#include "AI/AuraAIController.h"
+
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+AAuraAIController::AAuraAIController()
+{
+	Blackboard = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComponent"));
+	check(Blackboard);
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTreeComponent"));
+	check(BehaviorTreeComponent);
+}
