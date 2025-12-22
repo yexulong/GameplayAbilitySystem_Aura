860a392d03a134610e6d77aced74c470c0d1f1c8
wb.yexulong <wb.yexulong@mesg.corp.netease.com>
2025-12-22 20:48:28 +0800

    Finding the Nearest Player

--- Source/test3/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/test3/Private/AI/BTService_FindNearestPlayer.cpp
@@ -4,7 +4,10 @@
 #include "AI/BTService_FindNearestPlayer.h"
 
 #include "AIController.h"
+#include "VectorTypes.h"
+#include "BehaviorTree/BTFunctionLibrary.h"
 #include "Kismet/GameplayStatics.h"
+#include "Materials/MaterialExpressionOperator.h"
 
 void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
@@ -17,5 +20,22 @@ void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, u
 	TArray<AActor*> ActorsWithTag;
 	UGameplayStatics::GetAllActorsWithTag(OwningPawn, TargetTag, ActorsWithTag);
 
-	
+	float ClosestDistance = TNumericLimits<float>::Max();
+	AActor* ClosestActor = nullptr;
+	for (AActor* Actor : ActorsWithTag)
+	{
+		GEngine->AddOnScreenDebugMessage(2, .5f, FColor::Orange, *Actor->GetName());
+
+		if (IsValid(Actor) && IsValid(OwningPawn))
+		{
+			const float Distance = OwningPawn->GetDistanceTo(Actor);
+			if (Distance < ClosestDistance)
+			{
+				ClosestActor = Actor;
+				ClosestDistance = Distance;
+			}
+		}
+	}
+	UBTFunctionLibrary::SetBlackboardValueAsObject(this, TargetToFollowSelector, ClosestActor);
+	UBTFunctionLibrary::SetBlackboardValueAsFloat(this, DistanceToTargetSelector, ClosestDistance);
 }
